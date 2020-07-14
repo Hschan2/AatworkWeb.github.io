79293fb6fd9d3f78e2073aa3f357f5376ece957a
HONGSEONGCHAN <seongchan_@naver.com>
2020-07-14 18:02:31 +0900

    artworkWeb 수정

--- artworkWeb1/artworkWeb.js
+++ artworkWeb1/artworkWeb.js
@@ -4,10 +4,11 @@ var swiper = new Swiper('.swiper-container', {
   centeredSlides: true,
   slidesPerView: 'auto',
   coverflowEffect: {
-    rotate: 50,
+    rotate: 20,
     stretch: 0,
-    depth: 100,
+    depth: 200,
     modifier: 1,
     slideShadows: true,
   },
+  loop: true,
 });
