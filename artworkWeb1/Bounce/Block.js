b112be1192a49065f05176c2b005d1a333d50442
HONGSEONGCHAN <seongchan_@naver.com>
2020-07-29 19:54:07 +0900

    BounceBall

---
+++ artworkWeb1/Bounce/Block.js
@@ -0,0 +1,36 @@
+export class Block {
+    constructor(width, height, x, y) {
+        this.width = window;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.maxX = width + x;
+        this.maxY = height + y
+    }
+
+    draw(ctx) {
+        const xGap = 80;
+        const yGap = 60;
+
+        ctx.fillStyle = '#d18063';
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.fill();
+
+        ctx.fillStyle = '#f0e4d4';
+        ctx.beginPath();
+        ctx.moveTo(this.maxX, this.maxY);
+        ctx.lineTo(this.maxX - xGap, this.maxY + yGap);
+        ctx.lineTo(this.x - xGap, this.maxY + yGap);
+        ctx.lineTo(this.x, this.maxY);
+        ctx.fill();
+
+        ctx.fillStyle = '#f9d9ca';
+        ctx.beginPath();
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(this.x, this.maxY);
+        ctx.lineTo(this.x - xGap, this.maxY + yGap);
+        ctx.lineTo(this.x - xGap, this.maxY + yGap - this.height);
+        ctx.fill();
+    }
+}
