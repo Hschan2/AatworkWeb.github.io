5993a47f46cadd696605f95e3f4d706c627cb2c4
HONGSEONGCHAN <seongchan_@naver.com>
2020-08-04 21:50:36 +0900

    BounceBall

--- artworkWeb1/Bounce/Block.js
+++ artworkWeb1/Bounce/Block.js
@@ -1,6 +1,6 @@
 export class Block {
     constructor(width, height, x, y) {
-        this.width = window;
+        this.width = width;
         this.height = height;
         this.x = x;
         this.y = y;
