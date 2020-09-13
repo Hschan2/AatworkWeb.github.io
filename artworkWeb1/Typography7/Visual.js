6de49a89a7e3753d73d0cab77bd4f516fc2eb97e
HONGSEONGCHAN <seongchan_@naver.com>
2020-09-13 16:44:44 +0900

    Add a Typography v7

---
+++ artworkWeb1/Typography7/Visual.js
@@ -0,0 +1,52 @@
+import {Text} from './Text.js';
+import {BounceString} from './bounceStrings.js';
+
+// let inputString = prompt('알파벳 하나를 입력하세요.');
+// const eng = /^[a-zA-Z]*$/;
+// if(!eng.test(inputString) || inputString === '' || inputString === null) {
+//     location.reload();
+// }
+// if(inputString.length > 1) {
+//     location.reload();
+// }
+
+export class Visual {
+    constructor() {
+        this.text = new Text();
+
+        this.strings = [];
+
+        this.mouse = {
+            x: 0,
+            y: 0,
+            radius: 100,
+        };
+
+        document.addEventListener('pointermove', this.onMove.bind(this), false);
+    }
+
+    show(stageWidth, stageHeight) {
+        this.pos = this.text.setText('H', 5, stageWidth, stageHeight);
+
+        this.strings = [];
+        for(let i = 0; i < this.pos.outline.length; i++) {
+            this.strings[i] = new BounceString({
+                x1: this.pos.outline[i].x,
+                y1: this.pos.outline[i].minY,
+                x2: this.pos.outline[i].x,
+                y2: this.pos.outline[i].maxY,
+            });
+        }
+    }
+
+    animate(ctx) {
+        for(let i = 0; i < this.strings.length; i++) {
+            this.strings[i].animate(ctx, this.mouse.x, this.mouse.y);
+        }
+    }
+
+    onMove(e) {
+        this.mouse.x = e.clientX;
+        this.mouse.y = e.clientY;
+    }
+}
